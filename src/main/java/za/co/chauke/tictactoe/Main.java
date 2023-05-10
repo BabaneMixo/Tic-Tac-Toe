8a8bda31fb27e252ec4b15cd87c2e9f20e6c0d60
mibaban022 <mibaban022@student.wethinkcode.co.za>
2023-05-10 12:39:09 +0200

    tictac GUI

--- src/main/java/za/co/chauke/tictactoe/Main.java
+++ src/main/java/za/co/chauke/tictactoe/Main.java
@@ -2,7 +2,6 @@ package za.co.chauke.tictactoe;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        ;
         TicTacToe ticTacToe = new TicTacToe();
 
 
