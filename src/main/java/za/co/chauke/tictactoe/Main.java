5d5f5059197371d29ddc00db95f652162aa3bf2c
mibaban022 <mibaban022@student.wethinkcode.co.za>
2023-05-10 12:38:11 +0200

    tic

--- src/main/java/za/co/chauke/tictactoe/Main.java
+++ src/main/java/za/co/chauke/tictactoe/Main.java
@@ -2,7 +2,7 @@ package za.co.chauke.tictactoe;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        System.out.printf("Hello and welcome!");
+        ;
         TicTacToe ticTacToe = new TicTacToe();
 
 
